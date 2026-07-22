3e8fdf16a06ff3d2e522eba398284b6ca29bf362
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-07-22 18:59:23 -0500

    feat: impkementar clase nodo, con sus atributos y metodos segun el diagrama propuesto

--- src/iterator/Nodo.java
+++ src/iterator/Nodo.java
@@ -1,4 +1,23 @@
 package iterator;
+import model.Producto;
 
 public class Nodo {
+    private Producto producto;
+    private Nodo siguiente;
+
+    public Nodo(Producto producto){
+        this.producto = producto;
+    }
+
+    public Producto getProducto(){
+        return producto;
+    }
+
+    public Nodo getSiguiente(){
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente){
+        this.siguiente = siguiente;
+    }
 }
