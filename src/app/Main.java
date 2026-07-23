a53a29e0458aed97d56a9238cb10d68720abc8eb
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-07-22 20:34:13 -0500

    feat: implementar el main para que el iterator recorra todo el menu que dispone la cafeteria sin saber la estructura que lo hará

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,17 +1,22 @@
 package app;
+import iterator.*;
+import model.Producto;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        MenuCafeteria menu= new MenuCafeteria();
+        menu.addProducto(new Producto("Espresso", "Cafes", 6.5));
+        menu.addProducto(new Producto("Cappuccino", "Cafes", 8.0));
+        menu.addProducto(new Producto("Latte", "Cafes", 8.5));
+        menu.addProducto(new Producto("Te verde", "Tes", 5.0));
+        menu.addProducto(new Producto("Alfajor", "Postres", 4.5));
+        menu.addProducto(new Producto("Cheesecake", "Postres", 9.0));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Iterator iterador= menu.createIterator();
+        System.out.println("****** MENÚ CAFETERÍA *****");
+        while (iterador.hasNext()){
+            Producto producto = (Producto) iterador.next();
+            System.out.println(producto);
         }
     }
 }
