c3a2f16d122ee9a08b9e456931358ca51b2cb582
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-07-22 20:04:19 -0500

    feat: implementar clase MenuCafeteria con los metodos que implementa de la interface Agregate y sus atributos de la estructura de una lista

--- src/iterator/MenuCafeteria.java
+++ src/iterator/MenuCafeteria.java
@@ -1,4 +1,27 @@
 package iterator;
+import model.Producto;
 
-public class MenuCafeteria {
+public class MenuCafeteria implements Agregado{
+    private Nodo head;
+    private Nodo tail;
+
+    public MenuCafeteria(){
+    }
+
+    public void addProducto(Producto producto){
+        Nodo nuevo = new Nodo(producto);
+        if (head==null){
+            head= nuevo;
+            tail= nuevo;
+        }
+        else{
+            tail.setSiguiente(nuevo);
+            tail= nuevo;
+        }
+    }
+
+    @Override
+    public Iterator createIterator(){
+        return new LinkedIterator(head);
+    }
 }
