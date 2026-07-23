7c065629f268d6029b7f6b07b52d6340a2fdbf0b
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-07-22 19:37:24 -0500

    feat: implementar clase LinkedIterator con los metodos que implementa de la interface Iterator y su unico atributo, current

--- src/iterator/LinkedIterator.java
+++ src/iterator/LinkedIterator.java
@@ -1,4 +1,21 @@
 package iterator;
 
-public class LinkedIterator {
+public class LinkedIterator implements Iterator{
+    private Nodo current;
+
+    public LinkedIterator(Nodo head){
+        this.current = head;
+    }
+
+    @Override
+    public boolean hasNext(){
+        return current != null;
+    }
+
+    @Override
+    public Object next(){
+        Object producto= current.getProducto();
+        current= current.getSiguiente();
+        return producto;
+    }
 }
